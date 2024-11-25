ad8a9dfc5ec2d56b82b1e8acbf9affa787d38a17
Thien-Bao Bui <80117447+tbbui-732@users.noreply.github.com>
2024-11-25 14:43:05 -0500

    updated vertex shader to include other inputs

--- src/1.getting_started/7.textures/shaders/vertex.glsl
+++ src/1.getting_started/7.textures/shaders/vertex.glsl
@@ -1,19 +1,20 @@
-//#version 330 core
-//layout(location = 0) in vec3 aPosition;
-//layout(location = 1) in vec3 aColor;
-//layout(location = 2) in vec2 aTexCoord;
-//
-//out vec3 ourColor;
-//out vec2 ourTexCoord;
-//
-//void main() {
-//    gl_Position = vec4(aPosition, 1.0); 
-//    ourTexCoord = aTexCoord;
-//}
-
 #version 330 core
-layout (location = 0) in vec3 aPos;
-void main()
+layout(location = 0) in vec3 aPos;
+layout(location = 1) in vec3 aColor;
+layout(location = 2) in vec2 aTexCoord;
+
+out vec3 ourColor;
+out vec2 ourTexCoord;
+
+void main() 
 {
-   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+    gl_Position = vec4(aPos, 1.0); 
+    ourTexCoord = aTexCoord;
 }
+
+//#version 330 core
+//Layout (location = 0) in vec3 aPos;
+//Void main()
+//{
+//   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+//}
