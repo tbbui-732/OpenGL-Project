e17a2573b25fef37a8a112e22e123586397e2dee
Thien-Bao Bui <80117447+tbbui-732@users.noreply.github.com>
2024-11-25 15:55:07 -0500

    Blend two textures together

--- src/1.getting_started/7.textures/shaders/fragment.glsl
+++ src/1.getting_started/7.textures/shaders/fragment.glsl
@@ -4,9 +4,11 @@ out vec4 FragColor;
 in vec3 ourColor;
 in vec2 ourTexCoord;
 
-uniform sampler2D ourTexture;
+uniform sampler2D texture1;
+uniform sampler2D texture2;
 
 void main()
 {
-    FragColor = texture(ourTexture, ourTexCoord);
+    FragColor = mix(texture(texture1, ourTexCoord),
+                    texture(texture2, ourTexCoord), 0.2);
 }
