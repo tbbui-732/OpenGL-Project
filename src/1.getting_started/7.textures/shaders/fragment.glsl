5b764eba171a19b549f61d6a32b03ce9ddbc0563
Thien-Bao Bui <80117447+tbbui-732@users.noreply.github.com>
2024-11-25 17:14:15 -0500

    Set mix value to uniform

--- src/1.getting_started/7.textures/shaders/fragment.glsl
+++ src/1.getting_started/7.textures/shaders/fragment.glsl
@@ -6,10 +6,11 @@ in vec2 ourTexCoord;
 
 uniform sampler2D texture1;
 uniform sampler2D texture2;
+uniform float mixValue = 0.2;
 
 void main()
 {
     vec2 vertFlippedTexCoord = vec2(ourTexCoord.x*-1, ourTexCoord.y);
     FragColor = mix(texture(texture1, ourTexCoord),
-                    texture(texture2, vertFlippedTexCoord), 0.2);
+                    texture(texture2, vertFlippedTexCoord), mixValue);
 }
