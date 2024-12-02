5cb685bd727696ea719b91c3b12b04aa4fc87c38
Thien-Bao Bui <80117447+tbbui-732@users.noreply.github.com>
2024-12-02 12:47:24 -0500

    Hardcode matrix transformation values for testing purposes

--- src/1.getting_started/8.transformations/shaders/fragment.glsl
+++ src/1.getting_started/8.transformations/shaders/fragment.glsl
@@ -1,16 +1,24 @@
+//#version 330 core
+//out vec4 FragColor;
+//
+//in vec3 ourColor;
+//in vec2 ourTexCoord;
+//
+//uniform sampler2D texture1;
+//uniform sampler2D texture2;
+//float mixValue = 0.2;
+//
+//void main()
+//{
+//    vec2 vertFlippedTexCoord = vec2(ourTexCoord.x*-1, ourTexCoord.y);
+//    FragColor = mix(texture(texture1, ourTexCoord),
+//                    texture(texture2, vertFlippedTexCoord), mixValue);
+//}
+
 #version 330 core
 out vec4 FragColor;
 
-in vec3 ourColor;
-in vec2 ourTexCoord;
-
-uniform sampler2D texture1;
-uniform sampler2D texture2;
-uniform float mixValue = 0.2;
-
 void main()
 {
-    vec2 vertFlippedTexCoord = vec2(ourTexCoord.x*-1, ourTexCoord.y);
-    FragColor = mix(texture(texture1, ourTexCoord),
-                    texture(texture2, vertFlippedTexCoord), mixValue);
+    FragColor = vec4(1.0f, 1.0f, 0.0f, 1.0f);
 }
