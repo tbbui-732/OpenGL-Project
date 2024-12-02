c1287cf35cc232338c862e7a763a94269517b85e
Thien-Bao Bui <80117447+tbbui-732@users.noreply.github.com>
2024-12-02 14:55:28 -0500

    Apply transformations using matrices from CPU

--- src/1.getting_started/8.transformations/shaders/fragment.glsl
+++ src/1.getting_started/8.transformations/shaders/fragment.glsl
@@ -1,24 +1,15 @@
-//#version 330 core
-//out vec4 FragColor;
-//
-//in vec3 ourColor;
-//in vec2 ourTexCoord;
-//
-//uniform sampler2D texture1;
-//uniform sampler2D texture2;
-//float mixValue = 0.2;
-//
-//void main()
-//{
-//    vec2 vertFlippedTexCoord = vec2(ourTexCoord.x*-1, ourTexCoord.y);
-//    FragColor = mix(texture(texture1, ourTexCoord),
-//                    texture(texture2, vertFlippedTexCoord), mixValue);
-//}
-
 #version 330 core
 out vec4 FragColor;
 
+in vec3 ourColor;
+in vec2 ourTexCoord;
+
+uniform sampler2D texture1;
+uniform sampler2D texture2;
+vec2 vertFlippedTexCoord = vec2(ourTexCoord.x*-1, ourTexCoord.y);
+
 void main()
 {
-    FragColor = vec4(1.0f, 1.0f, 0.0f, 1.0f);
+    FragColor = mix(texture(texture1, ourTexCoord),
+                    texture(texture2, vertFlippedTexCoord), 0.2f);
 }
--- src/1.getting_started/8.transformations/shaders/vertex.glsl
+++ src/1.getting_started/8.transformations/shaders/vertex.glsl
@@ -1,31 +1,16 @@
-//#version 330 core
-//layout(location = 0) in vec3 aPos;
-//layout(location = 1) in vec3 aColor;
-//layout(location = 2) in vec2 aTexCoord;
-//
-//out vec3 ourColor;
-//out vec2 ourTexCoord;
-//
-//uniform mat4 transform;
-//
-//void main() 
-//{
-//    gl_Position = transform * vec4(aPos, 1.0f); 
-//    ourColor = aColor;
-//    ourTexCoord = aTexCoord;
-//}
-
 #version 330 core
 layout(location = 0) in vec3 aPos;
+layout(location = 1) in vec3 aColor;
+layout(location = 2) in vec2 aTexCoord;
 
-uniform mat4 transform = mat4(
-    1.0f, 0.0f, 0.0f, 0.0f,
-    0.0f, 1.0f, 0.0f, 0.0f,
-    0.0f, 0.0f, 1.0f, 0.0f,
-    0.0f, 0.0f, 0.0f, 1.0f
-);
+out vec3 ourColor;
+out vec2 ourTexCoord;
 
-void main()
+uniform mat4 transform;
+
+void main() 
 {
-    gl_Position = transform * vec4(aPos, 1.0f);
+    gl_Position = transform * vec4(aPos, 1.0f); 
+    ourColor = aColor;
+    ourTexCoord = aTexCoord;
 }
