1c6973cce1703c980736e018209880809485d46f
Thien-Bao Bui <80117447+tbbui-732@users.noreply.github.com>
2024-12-03 15:33:19 -0500

    Do not use transform uniform for now

--- src/1.getting_started/9.coordinates/shaders/vertex.glsl
+++ src/1.getting_started/9.coordinates/shaders/vertex.glsl
@@ -10,7 +10,8 @@ uniform mat4 transform;
 
 void main() 
 {
-    gl_Position = transform * vec4(aPos, 1.0f); 
+    //gl_Position = transform * vec4(aPos, 1.0f); 
+    gl_Position = vec4(aPos, 1.0f);
     ourColor = aColor;
     ourTexCoord = aTexCoord;
 }
