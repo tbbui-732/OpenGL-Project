0ae4a7e10ea6acf0dd233327ce5505d73519f219
Thien-Bao Bui <80117447+tbbui-732@users.noreply.github.com>
2025-01-08 17:32:37 -0500

    Perceived color based on combination of object and light color

--- src/2.lighting/12.colors/fragment.glsl
+++ src/2.lighting/12.colors/fragment.glsl
@@ -1,9 +1,10 @@
 #version 330 core
 out vec4 FragColor;
 
-in vec3 ourColor;
+uniform vec3 objectColor;
+uniform vec3 lightColor;
 
 void main()
 {
-    FragColor = vec4(0.0f, 0.0f, 0.0f, 1.0f);
+    FragColor = vec4(objectColor * lightColor, 1.0f);
 }
