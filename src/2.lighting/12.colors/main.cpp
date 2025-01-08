2d054ae892c503d1669780f2951b997e44007e9b
Thien-Bao Bui <80117447+tbbui-732@users.noreply.github.com>
2025-01-08 17:27:01 -0500

    Change background to a green color

--- src/2.lighting/12.colors/main.cpp
+++ src/2.lighting/12.colors/main.cpp
@@ -197,7 +197,7 @@ int main() {
 
         // set background
         // ------
-        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClearColor(0.0f, 0.5f, 0.2f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // TODO: What does this do?
 
         // camera
