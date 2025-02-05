d2ca03994b936e8dd6261f47f0e8b5a554d8f518
Thien-Bao Bui <80117447+tbbui-732@users.noreply.github.com>
2025-02-05 18:54:59 -0500

    Place all values inside of namespace

--- include/learnopengl/theme.h
+++ include/learnopengl/theme.h
@@ -4,42 +4,43 @@
 // NOTE: DELETE ONCE DONE USING! This is for LSP purposes only
 #include <vector>
 #include "../glm/glm.hpp"
-
-// enums
-enum PhongTheme { 
-    NORMAL,
-    HORROR,
-    FACTORY,
-    BIOCHEM
-};
-
-enum Color {
-    LIGHT_TAN,
-    DARK_GRAY,
-    LIGHT_GRAY,
-    BLACK,
-    WHITE,
-    RED,
-    LIGHT_RED,
-    LIGHT_YELLOW,
-    LIGHT_BLUE,
-    LIGHT_GREEN
-};
-
-const std::vector<std::vector<float>> rgbVals = {
-    {210.0, 180.0, 140.0}, // Light Tan 
-    {64.0, 64.0, 64.0},    // Dark Gray 
-    {211.0, 211.0, 211.0}, // Light Gray 
-    {0.0, 0.0, 0.0},       // Black 
-    {255.0, 255.0, 255.0}, // White 
-    {255.0, 0.0, 0.0},     // Red 
-    {255.0, 102.0, 102.0}, // Light Red 
-    {255.0, 255.0, 153.0}, // Light Yellow 
-    {173.0, 216.0, 230.0}, // Light Blue 
-    {144.0, 238.0, 144.0}  // Light Green 
-};
+#include "../learnopengl/shader.h"
 
 namespace Theme {
+    // enums
+    enum PhongTheme { 
+        NORMAL,
+        HORROR,
+        FACTORY,
+        BIOCHEM
+    };
+
+    enum Color {
+        LIGHT_TAN,
+        DARK_GRAY,
+        LIGHT_GRAY,
+        BLACK,
+        WHITE,
+        RED,
+        LIGHT_RED,
+        LIGHT_YELLOW,
+        LIGHT_BLUE,
+        LIGHT_GREEN
+    };
+
+    const std::vector<std::vector<float>> rgbVals = {
+        {210.0, 180.0, 140.0}, // Light Tan 
+        {64.0, 64.0, 64.0},    // Dark Gray 
+        {211.0, 211.0, 211.0}, // Light Gray 
+        {0.0, 0.0, 0.0},       // Black 
+        {255.0, 255.0, 255.0}, // White 
+        {255.0, 0.0, 0.0},     // Red 
+        {255.0, 102.0, 102.0}, // Light Red 
+        {255.0, 255.0, 153.0}, // Light Yellow 
+        {173.0, 216.0, 230.0}, // Light Blue 
+        {144.0, 238.0, 144.0}  // Light Green 
+    };
+
     inline void setBackgroundColor(glm::vec3& background, const PhongTheme theme);
     inline void setDirectionLightPhong();
     inline void setPointLightPhong();
@@ -69,8 +70,8 @@ namespace Theme {
         background = colorVec;
     }  
 
-    inline void setDirectionLightPhong() {
-
+    inline void setDirectionLightPhong(Shader& shaderProgram) {
+        
     }  
 
     inline void setPointLightPhong() {
