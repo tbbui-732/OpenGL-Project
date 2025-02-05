159fadac22a36397a7d5050dff1027d71e058f4b
Thien-Bao Bui <80117447+tbbui-732@users.noreply.github.com>
2025-02-05 18:53:31 -0500

    Create some methods for naemspace

--- include/learnopengl/theme.h
+++ include/learnopengl/theme.h
@@ -1,16 +1,31 @@
 #ifndef THEME_H
 #define THEME_H
 
+// NOTE: DELETE ONCE DONE USING! This is for LSP purposes only
+#include <vector>
+#include "../glm/glm.hpp"
+
 // enums
-enum PhongTheme { NORMAL, HORROR, FACTORY, BIOCHEM };
+enum PhongTheme { 
+    NORMAL,
+    HORROR,
+    FACTORY,
+    BIOCHEM
+};
 
-// declarations
-void setDirLight(); // TODO
-void setPointLight(); // TODO
-void setSpotLight(); // TODO
-void setBackground(); // TODO
+enum Color {
+    LIGHT_TAN,
+    DARK_GRAY,
+    LIGHT_GRAY,
+    BLACK,
+    WHITE,
+    RED,
+    LIGHT_RED,
+    LIGHT_YELLOW,
+    LIGHT_BLUE,
+    LIGHT_GREEN
+};
 
-// set up values for "colors"
 const std::vector<std::vector<float>> rgbVals = {
     {210.0, 180.0, 140.0}, // Light Tan 
     {64.0, 64.0, 64.0},    // Dark Gray 
@@ -24,42 +39,60 @@ const std::vector<std::vector<float>> rgbVals = {
     {144.0, 238.0, 144.0}  // Light Green 
 };
 
-class Theme {
-private:
-    std::vector<glm::vec3> colors;
-    Shader shaderProgram;
-    std::string dirLight; 
-    std::string pointLight;
-    std::string spotLight;
+namespace Theme {
+    inline void setBackgroundColor(glm::vec3& background, const PhongTheme theme);
+    inline void setDirectionLightPhong();
+    inline void setPointLightPhong();
+    inline void setSpotLightPhong();
+    inline glm::vec3 arrToVec(std::vector<float> arr);
+    inline void rgbToUnit(glm::vec3& vec);
 
-    void normalizeColor(glm::vec3& rgb) {
-        rgb.x /= 255.0f; 
-        rgb.y /= 255.0f; 
-        rgb.z /= 255.0f; 
-    }
-
-public:
-    Theme(  
-        const Shader& _shaderProgram, 
-        const std::string& _dirLight, 
-        const std::string& _pointLight, 
-        const std::string& _spotLight ) 
-    {
-        // set private variables
-        this->shaderProgram = _shaderProgram;
-        this->dirLight      = _dirLight;
-        this->pointLight    = _pointLight;
-        this->spotLight     = _spotLight;
-        
-        // update colors
-        colors.reserve(rgbVals.size());
-        for (std::vector<int> rgbVal : rgbVals) {
-            glm::vec3 color(rgbVal[0], rgbVal[1], rgbVal[2]);
-            normalizeColor(color);
-            colors.push_back(color);
+    inline void setBackgroundColor(glm::vec3& background, const PhongTheme theme) {
+        Color color;
+        switch (theme) {
+            case NORMAL:
+                color = DARK_GRAY;
+                break;
+            case HORROR:
+                color = BLACK;
+                break;
+            case FACTORY:
+                color = LIGHT_GRAY;
+                break;
+            case BIOCHEM:
+                color = WHITE;
+                break;
         }
-    }
-};
 
+        std::vector<float> colorArr = rgbVals[color];
+        glm::vec3 colorVec = arrToVec(colorArr);
+        background = colorVec;
+    }  
+
+    inline void setDirectionLightPhong() {
+
+    }  
+
+    inline void setPointLightPhong() {
+
+    }  
+
+    inline void setSpotLightPhong() {
+
+    }  
+
+    inline glm::vec3 arrToVec(std::vector<float> arr) {
+        glm::vec3 vec(arr[0], arr[1], arr[2]);
+        rgbToUnit(vec);
+        return vec;
+    }
+
+    inline void rgbToUnit(glm::vec3& vec) {
+        float scale = 255.0f;
+        vec.x /= scale;
+        vec.y /= scale;
+        vec.z /= scale;
+    }
+}
 
 #endif
