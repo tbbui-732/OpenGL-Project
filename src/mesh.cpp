81635c2ef92d840f60a240d80984097c645df4d0
Thien-Bao Bui <80117447+tbbui-732@users.noreply.github.com>
2025-02-06 12:59:59 -0500

    Write method for setting up mesh

--- src/mesh.cpp
+++ src/mesh.cpp
@@ -12,33 +12,67 @@ struct Texture {
 };
 
 class Mesh {
-    public:
-        // mesh data
-        std::vector<Vertex> vertices;
-        std::vector<unsigned int> indices;
-        std::vector<Texture> textures;
+private:
+    // render data
+    unsigned int VAO, VBO, EBO;
 
-        // ctor
-        Mesh(
-            std::vector<Vertex> vertices,
-            std::vector<unsigned int> indices,
-            std::vector<Texture> textures) {
+    void setupMesh() {
+        // set vertex array
+        glGenVertexArrays(1, &VAO);
 
-            this->vertices = vertices;
-            this->indices = indices;
-            this->textures = textures;
+        // set buffers
+        glGenBuffers(1, &EBO);
+        glGenBuffers(1, &VBO);
 
-        }
+        // bind vao
+        glBindVertexArray(VAO);
 
-        void Draw(Shader &shader) {
-            // TODO ...
-        }
+        // send data to vbo (?)
+        glBindBuffer(GL_ARRAY_BUFFER, VBO);
+        glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(Vertex), 
+                        &vertices[0], GL_STATIC_DRAW);
 
-    private:
-        // render data
-        unsigned int VAO, VBO, EBO;
+        // send data to ebo (?)
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int), 
+                        &indices[0], GL_STATIC_DRAW);
 
-        void setupMesh() {
-            // TODO ...
-        }
-}
+        // position
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)0);
+        glEnableVertexAttribArray(0);
+
+        // normal
+        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, Normal));
+        glEnableVertexAttribArray(1);
+
+        // texture
+        glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, TexCoord));
+        glEnableVertexAttribArray(2);
+
+        // unbind vao to prevent accidental state changes
+        glBindVertexArray(0);
+    }
+
+public:
+    // mesh data
+    std::vector<Vertex> vertices;
+    std::vector<unsigned int> indices;
+    std::vector<Texture> textures;
+
+    // ctor
+    Mesh(
+        std::vector<Vertex> vertices,
+        std::vector<unsigned int> indices,
+        std::vector<Texture> textures) {
+
+        this->vertices = vertices;
+        this->indices = indices;
+        this->textures = textures;
+
+        setupMesh();
+    }
+
+    void Draw(Shader &shader) {
+        // TODO ...
+    }
+};
