4cc6d27ca34a76ea25714043204fca92e460af2c
Thien-Bao Bui <80117447+tbbui-732@users.noreply.github.com>
2025-02-06 12:16:53 -0500

    Create a prototype of the Mesh class

--- src/mesh.cpp
+++ src/mesh.cpp
@@ -3,10 +3,42 @@
 struct Vertex {
     glm::vec3 Position;
     glm::vec3 Normal;
-    glm::vec3 TexCoord;
+    glm::vec2 TexCoord;
 };
 
 struct Texture {
     unsigned int id;
     std::string type;
 };
+
+class Mesh {
+    public:
+        // mesh data
+        std::vector<Vertex> vertices;
+        std::vector<unsigned int> indices;
+        std::vector<Texture> textures;
+
+        // ctor
+        Mesh(
+            std::vector<Vertex> vertices,
+            std::vector<unsigned int> indices,
+            std::vector<Texture> textures) {
+
+            this->vertices = vertices;
+            this->indices = indices;
+            this->textures = textures;
+
+        }
+
+        void Draw(Shader &shader) {
+            // TODO ...
+        }
+
+    private:
+        // render data
+        unsigned int VAO, VBO, EBO;
+
+        void setupMesh() {
+            // TODO ...
+        }
+}
