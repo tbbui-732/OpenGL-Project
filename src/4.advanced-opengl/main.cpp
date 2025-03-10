f901dcc8f0dcce104365b6b978fe1780b614d65f
Thien-Bao Bui <80117447+tbbui-732@users.noreply.github.com>
2025-03-10 12:27:07 -0400

    Clear stencil buffer every frame

--- src/4.advanced-opengl/main.cpp
+++ src/4.advanced-opengl/main.cpp
@@ -191,7 +191,7 @@ int main()
         // render
         // ------
         glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
 
         shader.use();
         glm::mat4 model = glm::mat4(1.0f);
